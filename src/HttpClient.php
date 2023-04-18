43c991fb4e65e56f9bf4018973a7477f2df906c3
Nic Rosental <nic@punchlist.com>
2023-04-17 21:06:57 -0400

    docblocked Http client

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -5,11 +5,21 @@ namespace Nicdev\WebflowSdk;
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Middleware;
+use Psr\Http\Message\ResponseInterface;
 
 class HttpClient
 {
     const BASE_URL = 'https://api.webflow.com';
 
+    /**
+     * Create a new HttpClient instance.
+     *
+     * @param string $token The API token to use.
+     * @param \GuzzleHttp\Client|null $client The Guzzle HTTP client to use.
+     * @param array $headers An array of headers to include in all requests.
+     * @param array $result The result of the last API request.
+     * @param array $history An array of HTTP transactions made by the client.
+     */
     public function __construct(
         private $token,
         private $client = null,
@@ -32,6 +42,13 @@ class HttpClient
         ]];
     }
 
+    /**
+     * Send a GET request to the API.
+     *
+     * @param string $path The path of the API endpoint.
+     * @param array $query An optional array of query parameters.
+     * @return array The decoded JSON response from the API.
+     */
     public function get($path, array $query = []): array
     {
         $response = $this->client->get($path, [...$this->headers, 'query' => $query]);
@@ -39,6 +56,13 @@ class HttpClient
         return $this->respond($response);
     }
 
+    /**
+     * Send a POST request to the API.
+     *
+     * @param string $path The path of the API endpoint.
+     * @param array $payload An optional array of data to include in the request body.
+     * @return array The decoded JSON response from the API.
+     */
     public function post($path, $payload = []): array
     {
         $response = $this->client->post($path, [...$this->headers, 'json' => $payload]);
@@ -46,6 +70,12 @@ class HttpClient
         return $this->respond($response);
     }
 
+    /**
+     * Send a DELETE request to the API.
+     *
+     * @param string $path The path of the API endpoint.
+     * @return array The decoded JSON response from the API.
+     */
     public function delete($path): array
     {
         $response = $this->client->delete($path, $this->headers);
@@ -53,6 +83,12 @@ class HttpClient
         return $this->respond($response);
     }
 
+    /**
+     * Send a PUT request to the API.
+     *
+     * @param string $path The path of the API endpoint.
+     * @return array The decoded JSON response from the API.
+     */
     public function put($path): array
     {
         $response = $this->client->put($path, $this->headers);
@@ -60,6 +96,12 @@ class HttpClient
         return $this->respond($response);
     }
 
+    /**
+     * Send a PATCH request to the API.
+     *
+     * @param string $path The path of the API endpoint.
+     * @return array The decoded JSON response from the API.
+     */
     public function patch($path): array
     {
         $response = $this->client->patch($path, $this->headers);
@@ -67,7 +109,13 @@ class HttpClient
         return $this->respond($response);
     }
 
-    public function respond($response): array
+    /**
+     * Process the response from the API and return the decoded JSON data.
+     *
+     * @param \Psr\Http\Message\ResponseInterface $response The HTTP response from the API.
+     * @return array The decoded JSON data from the response body.
+     */
+    public function respond(ResponseInterface $response): array
     {
         $this->result = json_decode($response->getBody(), true);
         if ($response->getStatusCode() === 200) {
@@ -76,6 +124,11 @@ class HttpClient
         return [];
     }
 
+    /**
+     * Create the Guzzle HTTP handler stack with a history middleware.
+     *
+     * @return array An array with the Guzzle HTTP handler stack.
+     */
     private function makeHandler(): array
     {
         $historyMiddleware = Middleware::history($this->history);
@@ -85,12 +138,22 @@ class HttpClient
         return ['handler' => $stack];
     }
 
+    /**
+     * Get the result of the last API request.
+     *
+     * @return array The decoded JSON data from the last API response.
+     */
     public function lastResult(): array
     {
         return $this->result;
     }
 
-    public function lastRequest()
+    /**
+     * Get the last HTTP request sent by the client.
+     *
+     * @return \Psr\Http\Message\RequestInterface|null The last HTTP request sent by the client, or null if there is no history.
+     */
+    public function lastRequest(): ?\Psr\Http\Message\RequestInterface
     {
         $request = end($this->history)['request'] ?? null;
         if ($request) {
@@ -98,7 +161,12 @@ class HttpClient
         }
     }
 
-    public function hasNextPage()
+    /**
+     * Check if there is a next page of results available.
+     *
+     * @return bool True if there is a next page of results, false otherwise.
+     */
+    public function hasNextPage(): bool
     {
         return isset($this->result['total'], $this->result['limit'], $this->result['offset']) &&
             ($this->result['total'] > $this->result['limit'] + $this->result['offset']);
