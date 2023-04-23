99d224d50d5adb985ec09ad9fdb3eaf33890ffc8
Nic Rosental <nic@punchlist.com>
2023-04-23 13:08:27 -0400

    wrapped up ecomm tests for now

--- tests/Feature/OrdersEndpointsTest.php
+++ tests/Feature/OrdersEndpointsTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
@@ -71,6 +72,65 @@ it('updates an order', function () {
         'Authorization' => ['Bearer foo'],
         'Accept' => ['application/json'],
     ]);
-    ray(json_decode($this->container[0]['request']->getBody()->getContents(), true));
-    // expect($this->container[0]['request']->getBody()->getContents())->toBe(json_encode($orderFields));
+    expect(json_decode($this->container[0]['request']->getBody()->getContents(), true))->toMatchArray([
+        'fields' => [
+            'comment' => 'Foo Bar',
+        ],
+    ]);
+});
+
+it('it can\'t update an invalid field for an order', function () {
+    $this->mockHandler->append(new Response(200, [], json_encode([])));
+    $orderFields = [
+        'not-valid-field' => 'Foo Bar',
+    ];
+    $this->webflowApiClient->updateOrder('foo', 'bar', $orderFields);
+})->throws(Exception::class);
+
+it('fulfills an order', function () {
+    $this->mockHandler->append(
+        new Response(200, [], json_encode([])),
+    );
+    
+    $data = $this->webflowApiClient->fulfillOrder('foo', 'bar', true);
+    expect($this->container[0]['request']->getMethod())->toBe('POST');
+    expect($this->container[0]['request']->getUri()->getPath())->toBe('/sites/foo/orders/bar/fulfill');
+    expect($this->container[0]['request']->getHeaders())->toMatchArray([
+        'Authorization' => ['Bearer foo'],
+        'Accept' => ['application/json'],
+    ]);
+    expect(json_decode($this->container[0]['request']->getBody()->getContents(), true))->toMatchArray([
+        'sendOrderFulfilledEmail' => true,
+    ]);
+
+    $this->mockHandler->reset();
+    $this->mockHandler->append(
+        new Response(200, [], json_encode([])),
+    );
+    $data = $this->webflowApiClient->fulfillOrder('foo', 'bar');
+    expect(json_decode($this->container[1]['request']->getBody()->getContents(), true))->toMatchArray([
+        'sendOrderFulfilledEmail' => false,
+    ]);
+});
+
+it('un-fulfills an order', function () {
+    $this->mockHandler->append(new Response(200, [], json_encode([])));
+    $data = $this->webflowApiClient->unfulfillOrder('foo', 'bar');
+    expect($this->container[0]['request']->getMethod())->toBe('POST');
+    expect($this->container[0]['request']->getUri()->getPath())->toBe('/sites/foo/orders/bar/unfulfill');
+    expect($this->container[0]['request']->getHeaders())->toMatchArray([
+        'Authorization' => ['Bearer foo'],
+        'Accept' => ['application/json'],
+    ]);
+});
+
+it('refunds an order', function () {
+    $this->mockHandler->append(new Response(200, [], json_encode([])));
+    $data = $this->webflowApiClient->refundOrder('foo', 'bar');
+    expect($this->container[0]['request']->getMethod())->toBe('POST');
+    expect($this->container[0]['request']->getUri()->getPath())->toBe('/sites/foo/orders/bar/refund');
+    expect($this->container[0]['request']->getHeaders())->toMatchArray([
+        'Authorization' => ['Bearer foo'],
+        'Accept' => ['application/json'],
+    ]);
 });
